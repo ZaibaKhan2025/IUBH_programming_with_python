caf9413e1c1d0996affe4591b054e4ad039a1087
zaiba khan <khanzaiba118@gmail.com>
2026-03-30 23:40:01 +0530

    Assignment - programming with python

---
+++ task.py
@@ -0,0 +1,236 @@
+import numpy as np
+import pandas as pd
+from scipy.optimize import curve_fit
+import mysql.connector
+from mysql.connector import Error
+import sys
+from bokeh.plotting import figure, show, output_file
+from bokeh.models import ColumnDataSource, HoverTool
+
+db_connection = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="",
+    database="python_assignment_zaiba_khan"
+)
+
+mycursor_1 = db_connection.cursor()
+mycursor_1.execute("show databases")
+databases = mycursor_1.fetchall()
+database_name = [i[0] for i in databases]
+
+if 'python_assignment_zaiba_khan' in database_name:
+    print(' ')
+else:
+    print('Database not exist, please read the instructions given !')
+    sys.exit()
+
+# CSV files to veriables
+def load_data(file_path):
+    data = pd.read_csv(file_path)
+    x_val = data['x'].values
+    y_cols = [col for col in data.columns if col.startswith('y')]
+    y_val = data[y_cols].values
+    return x_val, y_val
+
+# ideal function
+def ideal_function(x, a, b):
+    return a * x + b
+
+# Fit ideal function to training data
+def fit_ideal_functions(training_data, ideal_functions):
+    best_function_val = []
+    min_deviation = float('inf')
+    min_sum_squared_deviations_arry = []
+    x_train, y_train = training_data
+
+    j = 0
+    for y in y_train:
+        if j < 4:
+            for y_ideal in ideal_functions:
+                # Fit only the first column of y_train
+                params, _ = curve_fit(ideal_function, x_train, y_train[:, j])
+                y_pred = ideal_function(x_train, *params)
+                # Calculate deviation only for the first column
+                sum_squared_deviations = np.sum((y_train[:, j] - y_pred) ** 2)
+
+                if sum_squared_deviations < min_deviation:
+                    min_deviation = sum_squared_deviations
+                    best_function_params = params
+            j = j+1
+            best_function_val.append(best_function_params)
+            min_sum_squared_deviations_arry.append(min_deviation)
+    return best_function_val, min_sum_squared_deviations_arry
+    # put these values into the best_fit_function table
+
+# Map test data to chosen ideal functions
+def map_test_data(test_data, chosen_functions):
+    x_test, y_test = test_data
+    mappings = []
+
+    for x, y in zip(x_test, y_test):
+        deviations = []
+        for params in chosen_functions:
+            y_pred = ideal_function(x, *params)
+            deviation = np.abs(y - y_pred)
+            deviations.append(deviation)
+        min_deviation = min(deviations)
+        if min_deviation < np.sqrt(2) * np.max(deviations):
+            best_fit_index = deviations.index(min_deviation)
+            best_fit_params = chosen_functions[best_fit_index]
+            mappings.append((x, y, best_fit_params, min_deviation))
+        else:
+            mappings.append((x, y, None, None))
+    # print(mappings[0])
+    # sys.exit()
+    return mappings
+
+
+x_train, y_train = load_data("train.csv")  # training data (A)
+ideal_functions = load_data("ideal.csv")  # ideal data (C)
+x_test, y_test = load_data("test.csv")  # test data
+
+try:
+    train_data_list = [
+        (x, y1, y2, y3, y4)
+        for x, y1, y2, y3, y4 in zip(x_train, y_train[:, 0], y_train[:, 1], y_train[:, 2], y_train[:, 3])
+    ]
+    mycursor_1.executemany(
+        "INSERT INTO train (x, y1, y2, y3, y4) VALUES (%s, %s, %s, %s, %s)",
+        train_data_list
+    )
+    db_connection.commit()
+
+    test_data_list = [
+        (x, y)
+        for x, y in zip(x_test, y_test[:, 0])
+    ]
+    mycursor_1.executemany(
+        "INSERT INTO test (x, y) VALUES (%s, %s)",
+        test_data_list
+    )
+    db_connection.commit()
+
+    x_ideal, y_ideal = ideal_functions
+    ideal_data_list = [
+        (x, *y_row)
+        for x, y_row in zip(x_ideal, y_ideal)
+    ]
+    mycursor_1.executemany(
+        "INSERT INTO ideal (x, " + ", ".join(
+            [f"y{i+1}" for i in range(50)]) + ") VALUES (" + ", ".join(["%s"] * 51) + ")",
+        ideal_data_list
+    )
+    db_connection.commit()
+
+except Error as e:
+    # raise Exception("Sorry")
+    print(f"Something Happened {e}")
+finally:
+    print("xxxxxxxxxxxxxxxxx")
+
+# Choose the four best fitting ideal functions
+chosen_functions = fit_ideal_functions((x_train, y_train), ideal_functions)
+
+# Map test data to chosen ideal functions
+mappings = map_test_data((x_test, y_test), chosen_functions[0])
+
+print("Extrected ideal function Data :")
+print("S.No________x____________________y________________chosen_val")
+for i, params in enumerate(chosen_functions[0]):
+    print(f" {i+1}: {params[0]}, {params[1]}, {chosen_functions[1][i]}")
+    data_to_insert = [(params[0], params[1], chosen_functions[1][i])]
+    mycursor_1.executemany(
+        "INSERT INTO best_fit_func (x,y,choosen_func) VALUES (%s, %s, %s)", data_to_insert)
+db_connection.commit()
+
+print()
+print("Mappings Function :")
+print()
+print("_x____|___y____|____fit func [A]__|___fit func [B]___|___Deviation")
+for i, (x, y, best_fit_params, deviation) in enumerate(mappings):
+    if best_fit_params is not None:
+        print(x, y[0], best_fit_params[0], best_fit_params[1], deviation[0])
+        data_to_insert = [(x, y[0], best_fit_params[0],
+                           best_fit_params[1], deviation[0])]
+        mycursor_1.executemany(
+            "INSERT INTO mapping (x,y,ideal_x,ideal_y,deviation) VALUES (%s, %s, %s, %s, %s)", data_to_insert)
+    else:
+        print(f"Data point {
+              i+1}: x={x}, y={y}, No best fit found within the deviation threshold")
+db_connection.commit()
+
+# Graphing
+def plot_data_with_bokeh(x_train, y_train, x_test, y_test, chosen_functions, mappings):
+    try:
+        p = figure(title="Training Data, Ideal Functions, Test Data, and Mappings",
+                   x_axis_label='x', y_axis_label='y', width=800, height=600)
+
+        # Plot training data
+        for i in range(y_train.shape[1]):
+            p.scatter(x_train, y_train[:, i], legend_label=f'Training y{
+                      i+1}', color='green', size=6, alpha=0.6)
+
+        # Plot ideal functions
+        x_range = np.linspace(min(x_train), max(x_train), 500)
+        for i, params in enumerate(chosen_functions[0]):
+            y_range = ideal_function(x_range, *params)
+            p.line(x_range, y_range, legend_label=f'Ideal Function {
+                   i+1}', color='pink', line_width=2)
+
+        # Plot test data
+        for i in range(y_test.shape[1]):
+            p.scatter(x_test, y_test[:, i], legend_label=f'Test Data y{
+                      i+1}', color='black', size=6, alpha=0.6)
+
+        # Plot mappings with deviation
+        mapped_x = [x for x, y, function_idx,
+                    deviation in mappings if function_idx is not None]
+
+        mapped_y = [y for x, y, function_idx,
+                    deviation in mappings if function_idx is not None]
+        deviations = [deviation for x, y, function_idx,
+                      deviation in mappings if function_idx is not None]
+        source = ColumnDataSource(
+            data=dict(x=mapped_x, y=mapped_y, deviation=deviations))
+
+        p.scatter('x', 'y', source=source, color='red', size=10,
+                  alpha=0.6, legend_label='Mapped Test Data')
+
+        hover = HoverTool()
+        hover.tooltips = [("x", "@x"), ("y", "@y"),
+                          ("deviation", "@deviation")]
+        p.add_tools(hover)
+
+        p.legend.location = "top_left"
+        output_file("mapping.html")
+        show(p)
+    except Error as e:
+        print(f"some kind of error {e}")
+
+plot_data_with_bokeh(x_train, y_train, x_test, y_test,
+                     chosen_functions, mappings)
+
+def plot_data_xtrain_ytrain(x,y):
+    p = figure(title="Training Dataset", x_axis_label="x", y_axis_label="y")
+
+    p.line(x, y[:, 0], legend_label="x y1", color="yellow", line_width=2)
+    p.line(x, y[:, 1], legend_label="x y2", color="green", line_width=2)
+    p.line(x, y[:, 2], legend_label="x y3", color="blue", line_width=2)
+    p.line(x, y[:, 3], legend_label="x y4", color="red", line_width=2)
+
+    output_file("train.html")
+    show(p)
+plot_data_xtrain_ytrain(x_train,y_train)
+
+def plot_data_xtest_ytest(x,y):
+    
+    p = figure(title="Test Dataset", x_axis_label="x", y_axis_label="y")
+
+    p.line(x, y[:,0], legend_label="x y", color="yellow", line_width=2)
+    
+    output_file("test.html")
+    show(p)
+plot_data_xtest_ytest(x_test, y_test)
+
+print("XXXXXXXXXXXXXXXXXXXXXX")
